b8cbe787c3ed94b2f575c91e88cac627c333df8b
whopkins <will-hopkins@hotmail.co.uk>
2025-02-14 21:37:46 +0000

    Get run contexts from runner

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -16,6 +16,7 @@ import (
 // Runner provides capabilities to run GitHub actions
 type Runner interface {
 	NewPlanExecutor(plan *model.Plan) common.Executor
+	GetRunContexts() []*RunContext
 }
 
 // Config contains the config for a new runner
@@ -82,9 +83,14 @@ type caller struct {
 }
 
 type runnerImpl struct {
-	config    *Config
-	eventJSON string
-	caller    *caller // the job calling this runner (caller of a reusable workflow)
+	config      *Config
+	eventJSON   string
+	caller      *caller // the job calling this runner (caller of a reusable workflow)
+	RunContexts []*RunContext
+}
+
+func (runner *runnerImpl) GetRunContexts() []*RunContext {
+	return runner.RunContexts
 }
 
 // New Creates a new Runner
@@ -189,6 +195,7 @@ func (runner *runnerImpl) NewPlanExecutor(plan *model.Plan) common.Executor {
 
 				for i, matrix := range matrixes {
 					rc := runner.newRunContext(ctx, run, matrix)
+					runner.RunContexts = append(runner.RunContexts, rc)
 					rc.JobName = rc.Name
 					if len(matrixes) > 1 {
 						rc.Name = fmt.Sprintf("%s-%d", rc.Name, i+1)
