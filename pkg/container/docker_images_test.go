756ba734579e9712a9f484447e8febbbd09e03f4
whopkins <will-hopkins@hotmail.co.uk>
2026-02-18 16:19:20 +0000

    Log output

--- pkg/container/docker_images_test.go
+++ pkg/container/docker_images_test.go
@@ -1,67 +1,68 @@
 package container
 
 import (
-	"context"
-	"io"
-	"testing"
+    "context"
+    "io"
+    "testing"
 
-	"github.com/docker/docker/api/types/image"
-	"github.com/docker/docker/client"
-	log "github.com/sirupsen/logrus"
-	"github.com/stretchr/testify/assert"
+    "github.com/docker/docker/api/types/image"
+    "github.com/docker/docker/client"
+    log "github.com/sirupsen/logrus"
+    "github.com/stretchr/testify/assert"
 )
 
 func init() {
-	log.SetLevel(log.DebugLevel)
+    log.SetLevel(log.DebugLevel)
 }
 
 func TestImageExistsLocally(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping integration test")
-	}
-	ctx := context.Background()
-	// to help make this test reliable and not flaky, we need to have
-	// an image that will exist, and onew that won't exist
+    if testing.Short() {
+        t.Skip("skipping integration test")
+    }
+    ctx := context.Background()
+    // to help make this test reliable and not flaky, we need to have
+    // an image that will exist, and onew that won't exist
 
-	// Test if image exists with specific tag
-	invalidImageTag, err := ImageExistsLocally(ctx, "library/alpine:this-random-tag-will-never-exist", "linux/amd64")
-	assert.Nil(t, err)
-	assert.Equal(t, false, invalidImageTag)
+    // Test if image exists with specific tag
+    invalidImageTag, err := ImageExistsLocally(ctx, "library/alpine:this-random-tag-will-never-exist", "linux/amd64")
+    assert.Nil(t, err)
+    assert.Equal(t, false, invalidImageTag)
 
-	// Test if image exists with specific architecture (image platform)
-	invalidImagePlatform, err := ImageExistsLocally(ctx, "alpine:latest", "windows/amd64")
-	assert.Nil(t, err)
-	assert.Equal(t, false, invalidImagePlatform)
+    // Test if image exists with specific architecture (image platform)
+    invalidImagePlatform, err := ImageExistsLocally(ctx, "alpine:latest", "windows/amd64")
+    assert.Nil(t, err)
+    assert.Equal(t, false, invalidImagePlatform)
 
-	// pull an image
-	cli, err := client.NewClientWithOpts(client.FromEnv)
-	assert.Nil(t, err)
-	cli.NegotiateAPIVersion(context.Background())
+    // pull an image
+    cli, err := client.NewClientWithOpts(client.FromEnv)
+    assert.Nil(t, err)
+    cli.NegotiateAPIVersion(context.Background())
 
-	// Chose alpine latest because it's so small
-	// maybe we should build an image instead so that tests aren't reliable on dockerhub
-	readerDefault, err := cli.ImagePull(ctx, "node:16-buster-slim", image.PullOptions{
-		Platform: "linux/amd64",
-	})
-	assert.Nil(t, err)
-	defer readerDefault.Close()
-	_, err = io.ReadAll(readerDefault)
-	assert.Nil(t, err)
+    // Chose alpine latest because it's so small
+    // maybe we should build an image instead so that tests aren't reliable on dockerhub
+    readerDefault, err := cli.ImagePull(ctx, "node:16-buster-slim", image.PullOptions{
+        Platform: "linux/amd64",
+    })
+    assert.Nil(t, err)
+    defer readerDefault.Close()
+    _, err = io.ReadAll(readerDefault)
+    assert.Nil(t, err)
 
-	imageDefaultArchExists, err := ImageExistsLocally(ctx, "node:16-buster-slim", "linux/amd64")
-	assert.Nil(t, err)
-	assert.Equal(t, true, imageDefaultArchExists)
+    imageDefaultArchExists, err := ImageExistsLocally(ctx, "node:16-buster-slim", "linux/amd64")
+    assert.Nil(t, err)
+    assert.Equal(t, true, imageDefaultArchExists)
 
-	// Validate if another architecture platform can be pulled
-	readerArm64, err := cli.ImagePull(ctx, "node:16-buster-slim", image.PullOptions{
-		Platform: "linux/arm64",
-	})
-	assert.Nil(t, err)
-	defer readerArm64.Close()
-	_, err = io.ReadAll(readerArm64)
-	assert.Nil(t, err)
+    // Validate if another architecture platform can be pulled
+    readerArm64, err := cli.ImagePull(ctx, "node:16-buster-slim", image.PullOptions{
+        Platform: "linux/arm64",
+    })
+    assert.Nil(t, err)
+    defer readerArm64.Close()
+    b, err := io.ReadAll(readerArm64)
+    t.Log(string(b))
+    assert.Nil(t, err)
 
-	imageArm64Exists, err := ImageExistsLocally(ctx, "node:16-buster-slim", "linux/arm64")
-	assert.Nil(t, err)
-	assert.Equal(t, true, imageArm64Exists)
+    imageArm64Exists, err := ImageExistsLocally(ctx, "node:16-buster-slim", "linux/arm64")
+    assert.Nil(t, err)
+    assert.Equal(t, true, imageArm64Exists)
 }
